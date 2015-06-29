b28221982379872ddeef281c267ffdd2037ae886
shawnpan <spacexiao@163.com>
2015-06-30 00:20:09 +0800

    add

--- src/mytest/testString.go
+++ src/mytest/testString.go
@@ -6,5 +6,14 @@ import (
 
 func TestString() {
 	fmt.Println("testString -------------------")
+	fmt.Println("testString -------------------")
+	var s string = "abcdefg"
+	fmt.Println(s)
+
+	l := len(s)
+	fmt.Println(l)
+
+	ss := s[2:5]
+	fmt.Println(ss)
 
 }

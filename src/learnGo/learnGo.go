3c0baa30512c3e8c0554ec2605ebbff82fc0d63d
shawnpan <spacexiao@163.com>
2015-06-29 16:29:08 +0800

    modify to learnGo

---
+++ src/learnGo/learnGo.go
@@ -0,0 +1,41 @@
+// firstGo project main.go
+package main
+
+import (
+	"fmt"
+	"mytest"
+	"time"
+)
+
+const MAX_NUM int = 100
+const MIN_NUM = 0
+const MIDDLE_NUM = 50
+const (
+	SUNDAY = 1
+	MONDAY
+)
+
+func main() {
+
+	time.Sleep(10000)
+	//	mytest.TestMap()
+	//	mytest.TestArray()
+	mytest.TestStruct()
+	//	mytest.TestWeb()
+
+}
+
+func baseTest() {
+	fmt.Println("-----------baseTest-----------------")
+	i := 11.1
+	fmt.Println("Hello World!")
+	fmt.Println(i)
+	s := "aa"
+	fmt.Println(s)
+	fmt.Println(MAX_NUM)
+	fmt.Println(MIN_NUM)
+	fmt.Println(MIDDLE_NUM)
+	fmt.Println(MONDAY)
+	s1 := [10]int{1, 2, 3, 4, 5}
+	fmt.Println(s1)
+}

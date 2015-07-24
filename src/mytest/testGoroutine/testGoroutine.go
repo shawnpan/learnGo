c82a33f32da84e3b04e968b0de9f65c8199d44c1
shawnpan <spacexiao@163.com>
2015-07-24 18:04:44 +0800

    m testGoroutine

--- src/mytest/testGoroutine/testGoroutine.go
+++ src/mytest/testGoroutine/testGoroutine.go
@@ -2,29 +2,36 @@ package testGoroutine
 
 import (
 	"fmt"
+	"runtime"
 )
 
 var temp = 10
 var ch chan int = make(chan int)
 
 func TestGoroutine() {
-
-	fmt.Println("testGoroutine")
-	go func1()
-	go func2()
-	for i := 0; i < 2; i++ {
+	numCpu := runtime.NumCPU()
+	fmt.Println("cpu num : ", numCpu)
+	runtime.GOMAXPROCS(numCpu)
+	size := 10
+	for i := 0; i < size; i++ {
+		go func1()
+	}
+	for i := 0; i < 2*size; i++ {
+		go func2()
+	}
+	for i := 0; i < 3*size; i++ {
 		<-ch
 	}
+	fmt.Println(temp)
+	fmt.Println(1 << 20)
 }
 
 func func1() {
 	temp++
-	fmt.Println(temp)
 	ch <- 1
 }
 
 func func2() {
 	temp--
-	fmt.Println(temp)
 	ch <- 1
 }
